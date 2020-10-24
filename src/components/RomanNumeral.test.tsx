a631afc42976b569e85ae30680534aefa5fa5f27
Bek <galimbek88@gmail.com>
2020-10-24 18:35:58 +0100

    added test for 2 and 3

--- src/components/RomanNumeral.test.tsx
+++ src/components/RomanNumeral.test.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 class RomanNumeral {
 
     of(digit:number){
+        if (digit===3){
+            return "III"
+        }
+        if (digit===2){
+            return "II";
+        }
         return "I";
     }
 }
@@ -10,4 +16,6 @@ class RomanNumeral {
 test('roman numerals should', () => {
     let romanNumeral = new RomanNumeral();
     expect(romanNumeral.of(1)).toBe("I");
+    expect(romanNumeral.of(2)).toBe("II");
+    expect(romanNumeral.of(3)).toBe("III");
 });
