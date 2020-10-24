1ee4d4225d86b9741054cdc14266c4b6907769ce
edtheshed <edward.rixon@codurance.com>
2020-10-24 21:32:36 +0100

    refactored for simplicity

--- src/components/RomanNumeral.tsx
+++ src/components/RomanNumeral.tsx
@@ -3,33 +3,31 @@ export class RomanNumeral {
     of(digit: number) {
         let roman = ""
         let ones = digit % 10
-        let tens = ((digit - ones) % 100) / 10
-        let hundreds = ((digit - tens * 10 - ones) % 1000) / 100
-        let thousands = ((digit - hundreds * 100 - tens * 10 - ones) % 10000) / 1000
-        //  console.log(hundreds)
-        //  console.log(tens)
-        //  console.log(ones)
-        for (let index = 0; index < thousands; index++) {
+        let tens = (digit - ones) % 100
+        let hundreds = (digit - tens - ones) % 1000
+        let thousands = (digit - hundreds - tens - ones) % 10000
+
+        for (let index = 0; index < thousands / 1000; index++) {
             roman += "M"
             digit -= 1000
         }
         if (digit >= 500) {
             roman += "D"
             digit -= 500
-            hundreds -= 5
+            hundreds -= 500
         }
 
-        for (let index = 0; index < hundreds; index++) {
+        for (let index = 0; index < hundreds / 100; index++) {
             roman += "C"
             digit -= 100
         }
         if (digit >= 50) {
             roman += "L"
             digit -= 50
-            tens -= 5
+            tens -= 50
         }
 
-        for (let index = 0; index < tens; index++) {
+        for (let index = 0; index < tens / 10; index++) {
             roman += "X"
             digit -= 10
         }
@@ -41,6 +39,7 @@ export class RomanNumeral {
         for (let index = 0; index < ones; index++) {
             roman += "I"
         }
+
         return roman;
     }
 }
