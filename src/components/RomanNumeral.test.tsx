36df54c384e1edde95f3864c68ec80b1068023d9
Bek <galimbek88@gmail.com>
2020-10-24 19:52:20 +0100

    added 20

--- src/components/RomanNumeral.test.tsx
+++ src/components/RomanNumeral.test.tsx
@@ -8,4 +8,11 @@ test('roman numerals should', () => {
     expect(romanNumeral.of(3)).toBe("III");
     expect(romanNumeral.of(5)).toBe("V");
     expect(romanNumeral.of(6)).toBe("VI");
+    expect(romanNumeral.of(7)).toBe("VII");
+    expect(romanNumeral.of(8)).toBe("VIII");
+    expect(romanNumeral.of(10)).toBe("X");
+    expect(romanNumeral.of(11)).toBe("XI");
+    expect(romanNumeral.of(15)).toBe("XV");
+    expect(romanNumeral.of(16)).toBe("XVI");
+    expect(romanNumeral.of(20)).toBe("XX");
 });
--- src/components/RomanNumeral.tsx
+++ src/components/RomanNumeral.tsx
@@ -2,6 +2,14 @@ export class RomanNumeral {
 
     of(digit: number) {
         let roman = ""
+        let ones = digit%10
+        let tens = (digit-ones%100)/10
+        if (digit>=10){
+            for (let index = 0; index < tens; index++) {
+                roman+="X"
+               digit-=10
+            }             
+        }
         if (digit>=5){
             roman+="V"
             digit-=5
